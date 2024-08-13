6bd1e389fb856d0a7d289ac3307d63b731202392
isatyamks <isatyamks@gmail.com>
2024-08-13 19:51:47 +0530

    latest update

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ def signup():
 def home():
     username = request.args.get('username')
     room = request.args.get('room')
-    return render_template('home.html', username=username, room=room)
+
+    if username and room:
+        return render_template('home.html', username=username, room=room)
+    else:
+        return redirect(url_for('signup'))
 
 @socketio.on('send_message')
 def handle_send_message_event(data):
@@ -25,6 +29,5 @@ def handle_join_room_event(data):
     join_room(data['room'])
     socketio.emit('join_room_announcement', data, room=data['room'])
 
-
-
-
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
